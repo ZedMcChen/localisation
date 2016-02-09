c2f5b0e24cf34e40de124f2a3cef291cd8b9836a
ZedMcChen <zedmcchen@googlemail.com>
2016-02-09 15:29:30 +0000

    rename packages from zedmcchen to zhimingchen

---
+++ localisedtemplate/src/main/java/com/zhimingchen/websitelocalisation/localisedtemplate/config/WebApplicationInitializer.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright Zhiming Chen 2016
+ */
+package com.zhimingchen.websitelocalisation.localisedtemplate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author zhiming
+ *
+ */
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Configuration
+    public static class RootConfig {
+        
+    }
+    
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan("com.zhimingchen.websitelocalisation.localisedtemplate.controller")
+    public static class WebConfig extends WebMvcConfigurerAdapter {
+        @Bean
+        public ViewResolver viewResolver() {
+            InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+            
+            resolver.setPrefix("/WEB-INF/views/");
+            resolver.setSuffix(".jsp");
+            resolver.setExposeContextBeansAsAttributes(true);
+            return resolver;
+        }
+        
+        @Override
+        public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+            configurer.enable();
+        }
+        
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        }
+    }
+    
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+}
---
+++ singlelang/src/main/java/com/zhimingchen/websitelocalisation/singlelang/model/UserAuth.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright Zhiming Chen 2016
+ */
+package com.zhimingchen.websitelocalisation.singlelang.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * @author zhiming
+ *
+ */
+public class UserAuth {
+    @NotEmpty(message="email may not be empty")
+    @Email
+    private String email;
+    
+    @Size(min=8, message="password size must be at least 8")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
